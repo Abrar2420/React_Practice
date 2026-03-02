77c697f95f441673d258204d23ecac86e742ee2e
Abrar242002 <abrar242002@gmail.com>
2026-03-02 16:51:29 +0600

    confirmaiton commit

---
+++ 03reactProps/src/components/card.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Card({title, serial, price}) {
+
+console.log("props", title, serial, price);
+
+
+
+  return (
+    <>
+     <div className="w-60 flex flex-col rounded-xl bg-black min-h-[19rem] ">
+      <div>
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/ZkmdkuJUTLgJh96_FWQ5zweGGxo=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23084330/bored_ape_nft_accidental_.jpg"
+          alt="test"
+          className="object-cover object-center rounded-t-xl"
+        />
+      </div>
+      <div className="flex flex-col py-3 px-3 pb-10">
+        <div className="flex justify-between ">
+          <h3 className="font-bold text-">{title}</h3>
+          <h4>Price</h4>
+        </div>
+        <div className="flex  justify-between">
+          <p>{serial}</p>
+          <p>{price}</p>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Card
