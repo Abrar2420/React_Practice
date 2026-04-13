50a348bb35d0c46370c6eacee76ae67568ac0550
Abrar242002 <abrar242002@gmail.com>
2026-04-13 22:01:15 +0600

    fixes and changes

--- 02reactCounter/react_counter/src/App.jsx
+++ 02reactCounter/react_counter/src/App.jsx
@@ -1,30 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [counter, setCounter] = useState(5);
 
-let [counter, setCounter] = useState(5);
+  const add = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+      setCounter(counter + 1);
 
-const add = () => {
-  if(counter < 20) {
-    setCounter(counter + 1);
-  } 
-}
-const remove = () => {
-  if(counter > 0) {
-    setCounter(counter - 1);
-  } 
-  
-}
+      setCounter(counter + 1);
+
+      setCounter(counter + 1);
+      console.log(counter);
+    }
+  };
+  const remove = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
 
   return (
     <>
-    <h1>counter: {counter}</h1>     
-    <button onClick={add}>Increment</button>
-    <span> --- </span>
-    <button onClick={remove }>Decrement</button>
+      <h1>counter: {counter}</h1>
+      <button onClick={add}>Increment</button>
+      <span> --- </span>
+      <button onClick={remove}>Decrement</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
