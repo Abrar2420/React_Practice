d85b9f4c1d8fbefcdbd26f501ec98cdf768c4abd
Abrar242002 <abrar242002@gmail.com>
2026-04-14 17:16:05 +0600

    password generator, random password generated

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [elngth, setLength] = useState(8)
+  const [length, setLength] = useState(8)
+  const [numAllowed, setNumAllowed] = useState(true)
+  const [charAllowed, setCharAllowed] = useState(true)
+  const [password, setPassword] = useState("")
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = 
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(numAllowed) str += "0123456789"
+    if(numAllowed) str += "~!@#$%^&*(){}[]+-_*/"
+
+    while(pass.length < length) {
+      let randomCharIndex = Math.floor(Math.random() * str.length)
+      let randomChar = str[randomCharIndex]
+      pass += randomChar
+    }
+    // console.log(pass)
+    
+    
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [length, numAllowed, charAllowed, setPassword])
+  
 
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      <div className='w-full max-w-xl mx-auto shadow-md rounded-lg px-8 py-4 my-32 text-orange-500 bg-gray-700 shadow-md'>test</div>
     </>
   )
 }
